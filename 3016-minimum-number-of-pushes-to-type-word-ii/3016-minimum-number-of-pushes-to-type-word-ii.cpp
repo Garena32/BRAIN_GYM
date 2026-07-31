d22b91449cd31b67d64f2a1d2fa7117041ccac73
Prince IIT2024085 <garenatech232@gmail.com>
2026-07-31 18:14:43 +0530

    Time: 155 ms (5.28%), Space: 30.4 MB (5.99%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        vector<int> mpp(26, -1), freq(26, 0);
+
+        int cn = 1, cnt = 0;
+
+        map<char, int> mpp2;
+
+        
+
+        for ( char c : word ) mpp2[c]++;
+
+        for(auto it : mpp2){
+            freq[it.first-'a'] = it.second;
+        }
+
+        vector<pair<int, char>> nums;
+
+        for(int i=0; i<26; i++){
+            if(freq[i] > 0) nums.push_back({freq[i], i+'a'});
+        }
+
+        sort(nums.rbegin(), nums.rend());
+        
+        for(auto it : nums){
+
+            if(cnt == 8) {
+                cnt = 0;
+                cn++;
+            }
+
+            char c = it.second;
+
+            if(mpp[c-'a'] == -1){
+                mpp[c-'a'] = cn;
+                cnt++;
+            } 
+
+            
+
+            
+        }
+
+        for(auto it : nums) cout<<it.first << " " << it.second << "\n";
+
+
+        int ans = 0;
+
+        for(char c : word) ans += mpp[c-'a'];
+
+
+        return ans;
+    }
+};
