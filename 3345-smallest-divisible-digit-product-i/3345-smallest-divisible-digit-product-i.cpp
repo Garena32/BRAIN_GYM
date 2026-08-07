cba7287d2a412478339a1f406d77bef237a5b48e
Prince IIT2024085 <garenatech232@gmail.com>
2026-08-07 07:31:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (73.1%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+
+
+    bool ck(int n, int f){
+        int x = 1;
+        while(n!=0){
+            x *= n % 10;
+            n /= 10;
+        }
+        //cout << x%f;
+        
+        return x % f == 0;
+    }
+    
     int smallestNumber(int n, int t) {
+       for(int i=n; i<=n+10; i++){
+           if(ck(i, t)) {
+               return i;
+           }
+       }
+
+        return 404;
         
     }
 };
