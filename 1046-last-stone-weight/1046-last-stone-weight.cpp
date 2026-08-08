ec0d97906e78a6f005943a77fa856bf7a0830f5f
Prince IIT2024085 <garenatech232@gmail.com>
2026-08-08 22:50:25 +0530

    Time: 0 ms (100%), Space: 9.8 MB (80.37%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size(),ans;
+        priority_queue<int> pq;
+        for(int ele : stones) pq.push(ele);
+        while(!pq.empty()){
+            int a = pq.top();
+            pq.pop();
+            ans = a;
+            if(pq.empty()) break;
+            int b = pq.top();
+            pq.pop();
+
+            ans = ans - b;
+
+            pq.push(ans);
+        }
+        return ans;
+    }
+};
+
+// 1 1 2 4 1
