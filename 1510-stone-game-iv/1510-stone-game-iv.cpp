9539434ee5ebc25e7ce0c16d7d3d15a89b43aa29
Prince IIT2024085 <garenatech232@gmail.com>
2026-08-10 06:09:30 +0530

    Time: 43 ms (57.44%), Space: 14.1 MB (54.42%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+
+    using ll = long long;
+    int dp[100001];
+
+    bool solve(int n){
+        if(n == 0) return false;
+
+        if(dp[n] != -1) return dp[n];
+
+        
+        for(int i=1; i*i<=n; i++){
+            if(!solve(n-(i*i))) return dp[n] = true;
+        }
+
+        return dp[n] = false;
+    }
+public:
+    bool winnerSquareGame(int n) {
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
+    }
+};
