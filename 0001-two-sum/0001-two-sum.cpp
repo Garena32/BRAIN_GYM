f0d2d1542806b9add51c771133d151431bd603b8
Prince IIT2024085 <garenatech232@gmail.com>
2026-09-04 23:32:58 +0530

    Time: 0 ms (100%), Space: 14.8 MB (58.55%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        vector<pair<int, int>> arr;
+        for(int i=0; i<n; i++) arr.push_back({nums[i], i});
+
+        sort(arr.begin(), arr.end());
+
+        int i=0, j=n-1;
+        while(i<j){
+            int cs = arr[i].first+arr[j].first;
+            if(cs == target) return {arr[i].second, arr[j].second};
+            else if(cs < target) i++;
+            else j--;
+        }
+
+        return {-1, -1};
+    }
+};
